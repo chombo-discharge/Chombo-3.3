2aaf1549eea36b20c8a15749ee13c426c75be3f9
Robert Marskar <robert.marskar@gmail.com>
2023-06-16 14:45:21 +0200

    Added new constructor for EBISLevel which will do all the magic...

--- lib/src/EBTools/EBIndexSpace.cpp
+++ lib/src/EBTools/EBIndexSpace.cpp
@@ -650,10 +650,8 @@ EBIndexSpace::buildNextLevel(const GeometryService& a_geoserver, int a_nCellMax,
 
 #if 0
     m_ebisLevel[ilev] = new EBISLevel(*m_ebisLevel[ilev - 1], a_geoserver, a_nCellMax, a_fixRegularNextToMultiValued);
-
 #else
-    m_ebisLevel[ilev] =
-      new EBISLevel(m_domainLevel[ilev], m_origin, m_dx*std::pow(2, ilev),a_geoserver, a_nCellMax, a_fixRegularNextToMultiValued);
+    m_ebisLevel[ilev] = new EBISLevel(*m_ebisLevel[ilev - 1], a_geoserver, m_origin, a_nCellMax, a_fixRegularNextToMultiValued);    
 #endif
   }
 
